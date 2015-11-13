bcbb44e1cd69c21aeb55dbf0ab522b2d3f18ad94
akifo <akiho@bb07.net>
2015-11-14 02:12:31 +0900

    this is init

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
   'use strict';
 
+
+
   Vue.component('demo-grid', {
     template: '<table><tr v-for="station in data"><td v-for="info in station">{{info}}</td></tr></table>',
     props: {
@@ -12,10 +14,7 @@
   var app = new Vue({
     el: '#app',
     data: {
-      ranking_ken_1: [{
-        rank: 1,
-        name: 'branco'
-      }]
+      ranking_ken_1: []
     },
     created: function() {
       console.log('creted');
@@ -25,14 +24,28 @@
 
       getAjax: function() {
         var self = this;
-        $.get("https://docs.google.com/spreadsheets/d/1IkfDAo4WhcFN0n7OUqjvawz8kalKzcisVznA1HsXoH4/pub?gid=0&single=true&output=csv", function(csv){
-          console.log(csv);
-          $.csv.toArrays(csv,{}, function(err, data) {
-            console.log(data);
-            self.ranking_ken_1 = data;
-          });
+        var JSONURL = 'https://spreadsheets.google.com/feeds/list/1IkfDAo4WhcFN0n7OUqjvawz8kalKzcisVznA1HsXoH4/1/public/basic?alt=json';
+        $.get(JSONURL, function(spreadData){
+          self.cleanItUp(spreadData);
       	});
-      } // getAjax
+      }, // getAjax
+
+      cleanItUp: function(data) {
+        var self = this;
+        var cells = data.feed.entry;
+        _.map(cells, function(cell) {
+          console.log(cell);
+          var rowObj = {};
+          var rowCols = _.words(cell.content.$t,/[^,]+/g);
+          console.log(rowCols);
+          _.map(rowCols, function(col) {
+            var keyVal = _.words(col,/[^:]+/g);
+            rowObj[keyVal[0]] = keyVal[1];
+          });
+          rowObj.nid = cell.title.$t;
+          self.ranking_ken_1.push(rowObj);
+        });
+      }
 
     }
 
